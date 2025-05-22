5f78d28d2fd8bbf993832b3ab7bf7f07d408a7c5
thelegendofmario <ethanfrancis577@gmail.com>
2025-05-22 11:42:08 -0400

    feat: finished main loop(?)

--- src/escape_room/escape_room.py
+++ src/escape_room/escape_room.py
@@ -13,7 +13,10 @@ app = App(token=os.getenv('SLACK_BOT_TOKEN'))
 
 first_answer_encoded = ''
 first_answer = ''
+first_puzzle_complete = False
 has_key = False
+final_puzzle_answer = '07%2A846%235'
+second_puzzle_finished = False
 
 @app.message(re.compile("START"))
 def start_message(message, say):
@@ -51,8 +54,8 @@ def handle_begin(ack, say, body, respond):
 	first_answer_encoded = ''
 	ack()
 	respond(f"_<@{body['user']['id']}>_ started!")
-	say("_you are in a room, with a large vault door in front of you. there is a small sticky note on the door. it reads: 'GET OUT. -anonymous'_")
-	say("_to the right, there is a keypad on something that looks like a vault..._")
+	say("_you are in a room, with a large door with a large keypad on it in front of you. there is a small sticky note on the door. it reads: 'GET OUT. -anonymous'_")
+	say("_to the right, there is a 9-digit keypad on something that looks like a container of some kind..._")
 	say("_behind you there is a box with a keyhole._")
 	say("_as the sticky note says, GET OUT._")
 	say("_look around for clues, and have fun!_")
@@ -118,7 +121,7 @@ def handle_first_answer(message, say):
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": "_in cyber space, the paper resides here: https://hc-cdn.hel1.your-objectstorage.com/s/v3/756ed2af422576e762736dd33e4f0c46ac450c94_vwlu_zlzhtl.png. _"
+				"text": "_in cyberspace, the paper resides here: https://hc-cdn.hel1.your-objectstorage.com/s/v3/756ed2af422576e762736dd33e4f0c46ac450c94_vwlu_zlzhtl.png. _"
 			}
 		},
 		{
@@ -132,11 +135,12 @@ def handle_first_answer(message, say):
 
 @app.message("OPEN SESAME")
 def handle_second_puzzle_answer(say):
-	global first_puzzle_complete
+	global second_puzzle_finished
 	say("_you say the words into the empty room._")
 	say("_a panel in the wall that you didn't know existed pops out and reveals a secceret compartement._")
 	say("_inside the compartment there is a key._")
 	say("*type KEY to go over and pick up the key.*")
+	second_puzzle_finished = True
 
 @app.message(re.compile("KEY"))
 def handle_key_message(say):
@@ -151,13 +155,49 @@ def handle_key_message(say):
 def handle_second_clue(say):
 	global first_puzzle_complete
 	if first_puzzle_complete:
-		say("clue")
+		say("_try typing 'BOX'. that will lead you in the right direction..._")
 	else:
 		say("_You found this by accident! do this later_")
 
-@app.event("message")
-def handle_message_events(body, logger):
-    logger.info(body)
+@app.message("BOX")
+def handle_box(say):
+	global has_key, second_puzzle_finished
+	if has_key:
+		say("_you take the key and insert it into the box..._")
+		say("_the box opens and reveals a hastily drawn sticky note..._")
+		blocks = [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "_this is what it looks like:_"
+			}
+		},
+		{
+			"type": "image",
+			"image_url": "https://hc-cdn.hel1.your-objectstorage.com/s/v3/1c8c5e9bdbf50226168447978c41c0d9b4e8f77b_puzzle.png",
+			"alt_text": "a keypad with lines... this one you kinda need to see lol"
+		}
+		]
+		say(blocks=blocks, text="_this is what it looks like:_")
+		say("hmm... If I didn't know any better, this looks like a clue!")
+		say("if you solve it, type 'FINAL PUZZLE [code]'")
+	else:
+		say("_you look at the box..._")
+		say("_the box looks bare except for a single keyhole..._")
+
+# @app.message('FINAL PUZZLE 07*846#5')
+# def handle_final_answer(say):
+# 	say(":yay: you got out :yay:")
+
+@app.event('message')
+def handle_message_events(body, logger, say):
+	try:
+		if body['event']['text'] == 'FINAL PUZZLE 07*846#5':
+			say(":yay: you got out! :yay:")
+			say("thanks for playing!")
+	except Exception:
+		logger.info(body)
 
 
 def begin():
